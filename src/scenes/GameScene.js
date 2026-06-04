2d37c93a6b4eb95ac72d469616ba2785a40ef759
varus860 <bemnet860@gmail.com>
2026-06-04 17:18:02 +0300

    drawn player polygon

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -7,6 +7,25 @@ export default class GameScene extends Phaser.Scene {
 
   create() {
     console.log('GameScene create() fired');
+
+    // Create player texture
+    const width = 32;
+    const height = 32;
+    const g = this.make.graphics({ x: 0, y: 0, add: false });
+
+    g.fillStyle(0x00ffff, 1);
+    g.beginPath();
+    g.moveTo(16, 0);
+    g.lineTo(32, 32);
+    g.lineTo(0, 32);
+    g.closePath();
+    g.fillPath();
+
+    g.generateTexture('playerTexture', width, height);
+    g.destroy();
+
+    // Create player sprite
+    this.player = this.add.sprite(400, 300, 'playerTexture');
   }
 
   update() {
